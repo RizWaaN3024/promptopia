3e49780b779af20f34ed947c5ad1dcd17ac0ee32
Rizwaan <rizwaanans786@gmail.com>
2024-05-22 19:46:53 +0530

    Created schema and implemented authorization

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
-
+import { connectToDB } from '@utils/database';
+import User from "@models/user";
 const handler = NextAuth({
     providers: [
         GoogleProvider({
@@ -9,11 +10,31 @@ const handler = NextAuth({
         })
     ],
     async session({ session }) {
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
 
+        session.user.id = sessionUser._id.toString();
+        return session;
     },
     async signIn({ profile }) {
         try {
-            
+            await connectToDB();
+
+            // Check if a user already exists
+            const userExists = await User.findOne({
+                email:profile.email
+            });
+
+            // if not create a new user
+            if(!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username:profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture
+                })
+            }
+            return true;
         } catch (error) {
             
         }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,15 +7,15 @@ import {signIn, signOut, useSession, getProviders} from 'next-auth/react';
 
 const Nav = () => {
 
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
   const [ providers, setProviders ] = useState(null);
   const [ toggleDropdown, setToggleDropdown ] = useState(false);
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     }
-    setProviders();
+    setUpProviders();
   }, [])
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
@@ -23,16 +23,17 @@ const Nav = () => {
       <Image src="/assets/images/logo.svg" alt='promptopia-logo' width={30} height={30} className='object-contain'/>
       <p className='logo_text'>Promptopia</p>
       </Link>
+
       {/*Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href="/create-prompt" className='black_btn'>
               Create Post
             </Link>
             <button type='button' onClick={signOut} className='outline_btn'>Sign Out</button>
             <Link href="/profile">
-              <Image src="/assets/images/logo.svg" width={37} height={37} className='rounded-full' alt='profile' />
+              <Image src={session?.user.image} width={37} height={37} className='rounded-full' alt='profile' />
             </Link>
           </div>
         ) : (
@@ -49,9 +50,9 @@ const Nav = () => {
       </div>
       {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
-            <Image src="/assets/images/logo.svg" width={37} height={37} className='rounded-full' alt='profile' onClick={() => setToggleDropdown((prev) => !prev)}/>
+            <Image src={session?.user.image} width={37} height={37} className='rounded-full' alt='profile' onClick={() => setToggleDropdown((prev) => !prev)}/>
             {
               toggleDropdown && (
                 <div className='dropdown'>
