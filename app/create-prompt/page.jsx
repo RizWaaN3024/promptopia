63a97609ffe9ded794bec850a87854827933e1f4
Rizwaan <rizwaanans786@gmail.com>
2024-05-23 18:36:57 +0530

    Implemented Create Prompt Functionality

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,29 +1,51 @@
 "use client";
-import React from 'react';
-import { useState } from 'react';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import Form from '@components/Form';
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
 
 const CreatePrompt = () => {
-    const [ submitting, setSubmitting ] = useState(false);
-    const [post, setPost] = useState({
-        prompt:'',
-        tag:'',
-    });
+  const router = useRouter();
+  const { data: session } = useSession();
 
-    const createPrompt = async (e) => {
-      e.preventDefalt();
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
     }
-  return (
-    <Form 
-        type="Create"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={createPrompt}
-    />
-  )
-}
+  };
 
-export default CreatePrompt
+  return (
+    <Form
+      type='Create'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
+
+export default CreatePrompt;
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Feed = () => {
   return (
     <div>
-      
+      Feed
     </div>
   )
 }
